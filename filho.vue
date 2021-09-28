ec342a64a7af18d618e5dc959bf65b24b14bb46d
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-28 16:27:07 -0300

    Create filho.vue

---
+++ filho.vue
@@ -0,0 +1,26 @@
+<template>
+    {{ ViewModel }}
+</template>
+
+<script>
+import { watch } from "vue";
+
+export default {
+  
+    props: {
+        ViewModel: {
+            type: [String],
+            required: false,
+            default: "",
+        },
+    },
+    setup(props) {
+        console.log("vMOdel", props.ViewModel);
+        
+        watch(props, () => {
+            console.log("vMOdel mudou", props.ViewModel);
+        });
+    },
+
+};
+</script>
