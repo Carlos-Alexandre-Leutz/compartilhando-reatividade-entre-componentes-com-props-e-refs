7090fdcaf444d755770a0541e5fde608444d8962
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-28 16:26:46 -0300

    Create pai.vue

---
+++ pai.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <auto-numeric-input
+        :ViewModel="ViewModel"
+        ></auto-numeric-input>
+        <input v-model="ViewModel"/>:
+
+
+    </div>
+</template>
+ 
+<script>
+import AutoNumericInput from "./AutoNumericInput.vue"
+import { ref } from "vue";
+export default {
+    components:{
+        AutoNumericInput
+    },
+    setup() {
+        let ViewModel = ref();
+        return{
+            ViewModel
+        }
+    },
+};
+</script> 
