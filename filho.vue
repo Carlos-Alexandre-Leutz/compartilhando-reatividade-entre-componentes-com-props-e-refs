523061cb9e9a7e1230b31936fc2c58de68826d53
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-28 16:34:27 -0300

    Update filho.vue

--- filho.vue
+++ filho.vue
@@ -24,3 +24,53 @@ export default {
 
 };
 </script>
+/// outro ex com reatividade pai para filho e filho para pai com marcara monetaria no input
+<template>
+    <money3 v-model="valueInput" v-bind="config"></money3> {{ ViewModel }}
+</template>
+
+<script>
+import { Money3Component } from "v-money3";
+import { watch, ref } from "vue";
+
+export default {
+    components: { money3: Money3Component },
+    props: {
+        ViewModel: {
+            type: [String],
+            required: false,
+            default: "",
+        },
+    },
+    setup(props) {
+        let valueInput = ref();
+        
+        watch(props, () => {
+            console.log("vMOdel mudou", props.ViewModel);
+            valueInput.value = props.ViewModel
+        });
+        return{
+            valueInput
+        }
+    },
+    data() {
+        return {
+            amount: "12345.67",
+            config: {
+                masked: false,
+                prefix: "$",
+                suffix: "",
+                thousands: ",",
+                decimal: ".",
+                precision: 2,
+                disableNegative: false,
+                disabled: false,
+                min: null,
+                max: null,
+                allowBlank: false,
+                minimumNumberOfCharacters: 0,
+            },
+        };
+    },
+};
+</script>
